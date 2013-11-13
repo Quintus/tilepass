672246936f5a67a5948382d0fb4e84a37e72a121
Quintus <quintus@quintilianus.eu>
2013-11-13 19:16:17 +0100

    Add arrows to the arrow widget

--- src/arrow_tile.cpp
+++ src/arrow_tile.cpp
@@ -1,6 +1,24 @@
 #include "arrow_tile.hpp"
 #include "resource_manager.hpp"
 
+/****************************************
+Explanation of the widget’s pixel-layout:
+
+              /\     22px (arrow edge)
+              --      5px (distance between arrow and tile)
+           <  []  >  32px (tile edge, asummed)
+              --      5px (distance between arrow and tile)
+              \/      22px (arrow edge)
+
+Note the widget is perfectly symmetrical, so the values
+are the same for horizontal and vertical alignment.
+ ****************************************/
+
+// Pixel distances as layout out above
+static int s_tileedge      = 32; //px
+static int s_arrowedge     = 22; //px
+static int s_arrowdistance = 5;  //px
+
 /***************************************
  * Constructors & Destructors
  ***************************************/
@@ -8,13 +26,14 @@
 ArrowTile::ArrowTile()
   : Gtk::DrawingArea()
 {
-  mp_uparrow = gp_resource_manager->get_image("go-up.png");
-  mp_downarrow = gp_resource_manager->get_image("go-down.png");
-  mp_leftarrow = gp_resource_manager->get_image("go-previous.png");
+  mp_uparrow    = gp_resource_manager->get_image("go-up.png");
+  mp_downarrow  = gp_resource_manager->get_image("go-down.png");
+  mp_leftarrow  = gp_resource_manager->get_image("go-previous.png");
   mp_rightarrow = gp_resource_manager->get_image("go-next.png");
 
   // TODO: Make fixed size of 32px for tile configurable
-  set_size_request(86, 86);
+  int totaledge = s_tileedge + 2 * (s_arrowdistance + s_arrowedge);
+  set_size_request(totaledge, totaledge);
 }
 
 ArrowTile::~ArrowTile()
@@ -43,7 +62,18 @@ Glib::RefPtr<Gdk::Pixbuf> ArrowTile::get_tile()
 bool ArrowTile::on_draw(const Cairo::RefPtr<Cairo::Context>& cc)
 {
   if (mp_tile) {
-    Gdk::Cairo::set_source_pixbuf(cc, mp_tile, 0, 0);
+    Gdk::Cairo::set_source_pixbuf(cc, mp_uparrow, s_arrowedge + 2 * s_arrowdistance, 0);
+    cc->paint();
+    Gdk::Cairo::set_source_pixbuf(cc, mp_leftarrow, 0, s_arrowedge + 2 * s_arrowdistance);
+    cc->paint();
+    cc->set_source_rgb(1, 0, 0);
+    cc->rectangle(s_arrowedge + s_arrowdistance, s_arrowedge + s_arrowdistance, 32, 32);
+    cc->fill();
+    Gdk::Cairo::set_source_pixbuf(cc, mp_tile, s_arrowedge + s_arrowdistance, s_arrowedge + s_arrowdistance);
+    cc->paint();
+    Gdk::Cairo::set_source_pixbuf(cc, mp_rightarrow, s_arrowedge + 2 * s_arrowdistance + s_tileedge, s_arrowedge + 2 * s_arrowdistance);
+    cc->paint();
+    Gdk::Cairo::set_source_pixbuf(cc, mp_downarrow, s_arrowedge + 2 * s_arrowdistance, s_arrowedge + 2 * s_arrowdistance + s_tileedge);
     cc->paint();
 
     return true;
